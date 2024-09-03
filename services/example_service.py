a45ac2ccc4f70230384736d2f4f4cd6d0ed53755
Felipe Cortés <felipe@cortes.cl>
2024-09-03 02:49:58 -0400

    feat: add error type to example

--- services/example_service.py
+++ services/example_service.py
@@ -1,4 +1,7 @@
+import random
+
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
 router = APIRouter()
@@ -7,8 +10,18 @@ router = APIRouter()
 class User(BaseModel):
     username: str
 
-@router.get("/users")
-def read_users() -> list[User]:
+class Error(BaseModel):
+    detail: str
+
+@router.get(
+    "/users",
+    response_model=list[User],
+    responses={500: {"model": Error}},
+)
+def read_users():
+    if random.random() < 0.1:
+        return JSONResponse(status_code=500, content={"detail": "‽"})
+
     return [
         User(username="Ron"),
         User(username="Adi"),
